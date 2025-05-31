7e9e953b6ccaed6d16033c6a83022e6cb7ed53b3
Shivam awasthi <90971550+ShivamAwasthi1129@users.noreply.github.com>
2025-05-31 14:55:25 +0530

    minor change

--- app/package-shipment/page.jsx
+++ app/package-shipment/page.jsx
@@ -7,13 +7,9 @@ export const generateMetadata = () => {
 };
 
 const apiCall = async () => {
-<<<<<<< HEAD
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ship-env`, { cache: "no-store" });
-=======
     const response = await fetch("http://13.201.44.76/api/ship-env", {
       cache: "no-store",
     });
->>>>>>> 741517a7326581be69d3565e9647116001919cf3
   if (!response.ok) {
     throw new Error("Failed to fetch environment data (/api/ship-env).");
   }
@@ -26,5 +22,6 @@ const Shipment = async () => {
   const data = await apiCall();
   return <Package env={data} />;
 };
+
 
 export default Shipment;
