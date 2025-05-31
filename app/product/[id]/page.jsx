090594be0f8ae1ae0ffcd9376d77350bd1b9e23a
akkinet <akashsharma.zak@gmail.com>
2025-05-31 13:50:29 +0530

    offer price update

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -1,5 +1,5 @@
-import AdvancedProductDetail from '../../../components/client/AdvancedProductDetail'
-export const dynamic = 'force-dynamic';
+import AdvancedProductDetail from "../../../components/client/AdvancedProductDetail";
+export const dynamic = "force-dynamic";
 export const generateMetadata = () => {
   return {
     title: "Product Detail",
@@ -8,15 +8,23 @@ export const generateMetadata = () => {
 
 const fetchApi = async (id) => {
   let data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/product/${id}`, {
-    cache: 'no-store'
+    cache: "no-store",
   });
 
   return await data.json();
 };
 
+async function fetchShippingOffer() {
+  const response = await fetch("/api/ship-env", {
+    cache: "no-store",
+  });
+  return await response.json();
+}
+
 async function ProductDetailPage({ params }) {
   const data = await fetchApi(params.id);
+  const env = await fetchShippingOffer();
 
-  return <AdvancedProductDetail data={data} />
+  return <AdvancedProductDetail data={data} env={env} />;
 }
 export default ProductDetailPage;
